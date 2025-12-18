f419dd4eb9b7da3ce965454e587b67ea94f57f62
Kenneth Revne <kenneth@griffaviation.com>
2025-12-18 09:35:22 +0100

    expose xy noise parameters, for tuning KF outlier rejection for different sensors

--- libraries/AC_PrecLand/AC_PrecLand.cpp
+++ libraries/AC_PrecLand/AC_PrecLand.cpp
@@ -85,6 +85,22 @@ const AP_Param::GroupInfo AC_PrecLand::var_info[] = {
     // @User: Advanced
     AP_GROUPINFO("ACC_P_NSE", 6, AC_PrecLand, _accel_noise, 2.5f),
 
+    // @Param: XY_NSE_BASE
+    // @DisplayName: Kalman Filter Base XY Noise
+    // @Description: Kalman Filter Minimum lateral position noise. Sets the lowest allowed XY noise at low altitude
+    // @Units: m
+    // @Range: 0.0001 2.5
+    // @User: Advanced
+    AP_GROUPINFO("XY_NSE_BASE", 6, AC_PrecLand, _xy_pos_nse_base, 0.02f),
+
+    // @Param: XY_NSE_GROWTH
+    // @DisplayName: Kalman Filter XY Noise Growth
+    // @Description: Kalman Filter XY Position Noise gradient, XY Noise increases linearly with z height above target
+    // @Units: m/m
+    // @Range: 0 - 0.05
+    // @User: Advanced
+    AP_GROUPINFO("XY_NSE_GROWTH", 6, AC_PrecLand, _xy_pos_nse_growth, 0.01f),
+
     // @Param: CAM_POS_X
     // @DisplayName: Camera X position offset
     // @Description: X position of the camera in body frame. Positive X is forward of the origin.
@@ -550,7 +566,7 @@ void AC_PrecLand::run_estimator(float rangefinder_alt_m, bool rangefinder_alt_va
 
             // Update if a new Line-Of-Sight measurement is available
             if (construct_pos_meas_using_rangefinder(rangefinder_alt_m, rangefinder_alt_valid)) {
-                float xy_pos_var = sq(_target_pos_rel_meas_NED.z*(0.01f + 0.01f*AP::ahrs().get_gyro().length()) + 0.02f);
+                float xy_pos_var = sq(_target_pos_rel_meas_ned_m.z*(_xy_pos_nse_growth * + 0.01f*AP::ahrs().get_gyro().length() + _xy_pos_nse_base));
                 if (!_estimator_initialized) {
                     // Inform the user landing target has been found
                     GCS_SEND_TEXT(MAV_SEVERITY_INFO, "PrecLand: Target Found");
--- libraries/AC_PrecLand/AC_PrecLand.h
+++ libraries/AC_PrecLand/AC_PrecLand.h
@@ -203,6 +203,8 @@ private:
     AP_Float                    _land_ofs_cm_x;     // Desired landing position of the camera forward of the target in vehicle body frame
     AP_Float                    _land_ofs_cm_y;     // Desired landing position of the camera right of the target in vehicle body frame
     AP_Float                    _accel_noise;       // accelerometer process noise
+    AP_Float                    _xy_pos_nse_base;       // xy position noise minimum
+    AP_Float                    _xy_pos_nse_growth;     // xy position noise growth (per m of altitude)
     AP_Vector3f                 _cam_offset;        // Position of the camera relative to the CG
     AP_Float                    _xy_max_dist_desc;  // Vehicle doing prec land will only descent vertically when horizontal error (in m) is below this limit
     AP_Int8                     _strict;            // PrecLand strictness
